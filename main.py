23ba07ec08f036f78bbe0f30b2f547792d0dbb5b
roboevt <70998278+roboevt@users.noreply.github.com>
2021-03-06 11:59:29 -0600

    Better code with command prefix.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='test')
+@bot.command(name='test', help='responds with "test success!" if the bot is running correctly.')
 async def test(ctx):
     await ctx.send('test success!')
 
