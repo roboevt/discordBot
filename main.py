fc38fb5dad955a229848d8d23c1582aef4a7f3ae
roboevt <70998278+roboevt@users.noreply.github.com>
2021-03-25 13:32:40 -0500

    Creating events does not quite work.

--- main.py
+++ main.py
@@ -7,39 +7,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from Event import Event
+from Rules import Rules
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
-years = {1997: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/1997_dbf_rules.pdf',
-         1998: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/1998_dbf_rules.pdf',
-         1999: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/1999_dbf_rules.pdf',
-         2000: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2000_dbf_rules.pdf',
-         2001: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2001_dbf_rules.pdf',
-         2002: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2002_dbf_rules.pdf',
-         2003: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2003_dbf_rules.pdf',
-         2004: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2005-rules.pdf',
-         2005: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2005-rules.pdf',
-         2006: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2006_dbf_rules.pdf',
-         2007: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2007_dbf_rules.pdf',
-         2008: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2008_dbf_rules.pdf',
-         2009: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2009_dbf_rules.pdf',
-         2010: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2010_dbf_rules.pdf',
-         2011: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2011_dbf_rules.pdf',
-         2012: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2012_dbf_rules.pdf',
-         2013: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2013_dbf_rules.pdf',
-         2014: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2014_dbf_rules.pdf',
-         2015: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2015_dbf_rules.pdf',
-         2016: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/2016_dbf_rules.pdf',
-         2017: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/dbf-rules-2017.pdf',
-         2018: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/dbf-rules-2018.pdf',
-         2019: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/dbf-rules-2019.pdf',
-         2020: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/dbf-rules-2020.pdf',
-         2021: 'https://github.com/WUDBF/WUDBF2021/blob/master/Rules%20Archive/dbf-rules-2021-final-v01.pdf'
-         }
-list_of_events = []
-
 
 @bot.command(name='test2', help='responds with "test success!" if the bot is running correctly.')
 async def test(ctx):
@@ -50,8 +23,8 @@ async def test(ctx):
 async def test(ctx, year):
     if year.isdigit():
         year = int(year)
-        if year in years:
-            await ctx.send(years[year])
+        if year in Rules.years:
+            await ctx.send(Rules.years[year])
         else:
             await ctx.send('We do not have the year ' + str(year) + ' in our database, please try another year.')
     else:
@@ -60,17 +33,16 @@ async def test(ctx, year):
 
 @bot.command(name='create', help='Creates an event in future. Input a message to be sent, and when to send it')
 async def reminder(ctx, message, year, month, day, hour, minute):
-    event = Event.fromDateArgs(message, year, month, day, hour, minute)
-    print(event.time)
-    if event.secondsLeft() < 0:
-        await ctx.send('The time is in the past. Please choose a time that has not yet passed.')
+    event = Event.checkArgs(message, year, month, day, hour, minute)  # returns an error message or an Event object
+    if isinstance(event, str):
+        await ctx.send(event)
     else:
-        await ctx.send('message received, sending follow up after ' + str(round(event.secondsLeft(),0)) + ' seconds')
+        await ctx.send('message received, sending follow up after ' + str(round(event.secondsLeft(), 0)) + ' seconds')
         await delayedSend(ctx, event)
 
 
 async def delayedSend(ctx, event):
-    list_of_events.append(event)
+    Event.list_of_events.append(event)
     await asyncio.sleep(event.secondsLeft())
     await ctx.send(event.message)
 
--- venv/Event.py
+++ venv/Event.py
@@ -1,38 +1,51 @@
 from datetime import datetime
 
 class Event:
+    list_of_events = []
     def __init__(self, message, time):
         self.message=message
         self.time=time
 
     @classmethod
     def fromDateArgs(event, message, year, month, day, hour, minute):
-        valid=True
+        return event(message, datetime(year, month, day, hour, minute, 0))
+
+    @staticmethod
+    def checkArgs(message, year, month, day, hour, minute):
         if year.isdigit:
             year=int(year)
         else:
-            valid=False
+            return 'You must enter an integer for year.'
         if month.isdigit:
             month=int(month)
+            if not 0<=month<=12:
+                return 'The month must be from 0 to 12.'
         else:
-            valid = False
+            return 'You must enter an integer for month'
         if day.isdigit:
             day=int(day)
+            if not 0<=day<=31:
+                return 'The day must be from 0 to 31.'
         else:
-            valid = False
+            return 'You must enter an integer for day.'
         if hour.isdigit:
             hour=int(hour)
+            if not 0<=hour<=24:
+                return 'The hour must be from 0 to 24.'
         else:
-            valid = False
+            return 'You must enter an integer for hour.'
         if minute.isdigit:
             minute=int(minute)
+            if not 0<=minute<=60:
+                return 'The minute must be from 0 to 60.'
         else:
-            valid = False
-        if valid:
-            return event(message, datetime(year, month, day, hour, minute, 0))
-        else:
-            print("Invalid date entered.")
+            return 'You must enter an integer for minute.'
 
+        time=datetime(year, month, day, hour, minute, 0)
+        if time.timestamp() - datetime.now().timestamp() < 0:
+            return 'That time is in the past, please enter a time in the future.'
+
+        return Event(message, year, month, day, hour, minute)
 
     def secondsLeft(self):
         return self.time.timestamp() - datetime.now().timestamp()
